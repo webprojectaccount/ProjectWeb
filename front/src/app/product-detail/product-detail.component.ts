72ac3e82d251caa15babc72c6a1b4845d1d7ee66
webprojectaccount <81443432+webprojectaccount@users.noreply.github.com>
2021-04-27 20:46:49 +0600

    Update product-detail.component.ts

--- front/src/app/product-detail/product-detail.component.ts
+++ front/src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; f
 import { ActivatedRoute } from '@angular/router';
 import { CartService } from '../cart.service';
 import {ProductService} from '../product.service';
